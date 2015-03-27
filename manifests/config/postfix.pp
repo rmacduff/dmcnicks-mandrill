dc83f4a8ed07d84d7835fb1a9dcbd3f2d1e15c58
David McNicol <david@mcnicks.org>
2015-03-27 09:13:05 +0000

    Changed postfix config to use main.cf template rather than execs of postconf

--- manifests/config/postfix.pp
+++ manifests/config/postfix.pp
@@ -17,70 +17,35 @@ class mandrill::config::postfix (
   $apikey
 ) {
 
-  $sasl_passwd = 'hash:/etc/postfix/sasl_passwd'
-  $ca_certificates = '/etc/ssl/certs/ca-certificates.crt'
-
   package { 'postfix':
     ensure => 'present'
-  } ->
-
-  exec { 'inet_interfaces':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'inet_interfaces = 127.0.0.1'"
-  } ->
-
-  exec { 'myhostname':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'myhostname = ${mail_domain}'"
-  } ->
-
-  exec { 'mydestination':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'mydestination = ${mail_domain}, localhost'"
-  } ->
-
-  exec { 'relayhost':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'relayhost = [smtp.mandrillapp.com]'"
-  } ->
-
-  exec { 'smtp_sasl_auth_enable':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'smtp_sasl_auth_enable = yes'"
-  } ->
-
-  exec { 'smtp_sasl_password_maps':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'smtp_sasl_password_maps = ${sasl_passwd}'"
-  } ->
-
-  exec { 'smtp_sasl_security_options':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'smtp_sasl_security_options = noanonymous'"
-  } ->
-
-  exec { 'smtp_use_tls':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'smtp_use_tls = yes'"
-  } ->
-
-  exec { 'smtp_tls_CAfile':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => "postconf -e 'smtp_tls_CAfile = ${ca_certificates}'"
-  } ~>
+  }
 
   service { 'postfix':
-    ensure => 'running',
+    ensure  => 'running',
+    require => Package['postfix']
+  }
+
+  file { 'main.cf':
+    ensure  => 'present',
+    path    => '/etc/postfix/main.cf',
+    content => template('mandrill/postfix/main.cf.erb'),
+    require => Package['postfix'],
+    notify  => Service['postfix']
   }
 
   file { 'sasl_passwd':
     ensure  => 'present',
     path    => '/etc/postfix/sasl_passwd',
-    content => template('mandrill/postfix/sasl_passwd.erb')
-  } ->
+    content => template('mandrill/postfix/sasl_passwd.erb'),
+    require => Package['postfix'],
+    notify  => Exec['sasl_passwd.db']
+  }
 
   exec { 'sasl_passwd.db':
     path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => 'postmap /etc/postfix/sasl_passwd'
+    command => 'postmap /etc/postfix/sasl_passwd',
+    require => Package['postfix'],
+    notify  => Service['postfix']
   }
 }
